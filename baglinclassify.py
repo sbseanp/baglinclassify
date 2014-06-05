de28bbc309b223bf6d820120c46b93eb26077761
sbseanp <seanp.usa@gmail.com>
2014-06-04 22:40:55 -0700

    finished verbose classifier

--- baglinclassify.py
+++ baglinclassify.py
@@ -27,7 +27,23 @@ def l2_norm(vec):
     '''Compute the L2 norm of the vector vec'''
     return sqrt(sum([vec[i]**2 for i in range(len(vec))]))
 
-def verbose_classifier(filename, T, size):
+def print_bootstrap(sets):
+    '''Print bootstrap sample sets'''
+    for i in range(len(sets)):
+        print("Boostrap sample set",end=' ')
+        print(i+1,end='')
+        print(':')
+        for j in range(len(sets[i])):
+            for k in range(len(sets[i][j])-1):
+                print(sets[i][j][k],end=' ')
+            print('-',end=' ')
+            if sets[i][j][-1] == 1:
+                print("True")
+            else:
+                print("False")
+        print()
+
+def train_classifier(filename, T, size):
     '''Read a training file and learn the parameters for each class'''
     training_file = open(filename)
     
@@ -76,13 +92,18 @@ def verbose_classifier(filename, T, size):
         t = (l2_norm(true_centroid)**2 - l2_norm(false_centroid)**2)/2.0
         w.append(t)
         W.append(w) 
-    print(W)
         
-    return W
+    package = []
+    package.append(W)
+    package.append(total_set)    
+    return package
 
-def verbose_test_classifier(filename, W):
+def verbose_test_classifier(filename, input):
     testing_file = open(filename)
     
+    W = input[0]
+    bootstrap = input[-1]
+    class_list = []
     D, nTrue, nFalse = map(int, testing_file.readline().split())
     tp, fp, tn, fn = 0,0,0,0
     count = 0
@@ -100,27 +121,119 @@ def verbose_test_classifier(filename, W):
             actual_class = 0
         else:
             actual_class = 1
-            
+        # 0 = true, 1 = false; 0 = correct, 1 = false positive, 2 = false negative
         if actual_class == 0:
             if vote >= 0:
                 tp+=1
+                x[-1] = 0
+                x.append(0)
             else:
                 fp+=1
+                x[-1] = 1
+                x.append(1)
         else:
             if vote >= 0:
                 fn+=1
+                x[-1] = 0
+                x.append(2)
             else:
                 tn+=1
-                
+                x[-1] = 1
+                x.append(0)
+        class_list.append(x)
+        count+=1
+        
+    testing_file.close()
+    
     print("Positive examples:",end=' ')
     print(tp+fp)
     print("Negative examples:",end=' ')
-            
+    print(tn+fn)
+    print("False positives:",end=' ')
+    print(fp)
+    print("False negatives:",end=' ')
+    print(fn)
+    print()
     
+    print_bootstrap(bootstrap)
+    
+    print("Classification:")
+    for i in range(len(class_list)):
+        for j in range(len(class_list[i])-2):
+            print(class_list[i][j],end=' ')
+        print("-",end=' ')
+        if class_list[i][-2] == 0:
+            print("True (",end='')
+        else:
+            print("False (",end='')
+        if class_list[i][-1] == 0:
+            print("correct)")
+        elif class_list[i][-1] == 1:
+            print("false positive)")
+        else:
+            print("false negative)")
+            
+def normal_test_classifier(filename, input):
+    testing_file = open(filename)
+    
+    W = input[0]
+    class_list = []
+    D, nTrue, nFalse = map(int, testing_file.readline().split())
+    tp, fp, tn, fn = 0,0,0,0
+    count = 0
+    for line in testing_file:
+        x = list(map(float, line.split()))
+        x.append(-1)
+        vote = 0
+        for i in range(len(W)):
+            if dot_product(W[i], x) >= 0:
+                vote += 1
+            else:
+                vote += -1
+        
+        if count < nTrue:
+            actual_class = 0
+        else:
+            actual_class = 1
+        # 0 = true, 1 = false; 0 = correct, 1 = false positive, 2 = false negative
+        if actual_class == 0:
+            if vote >= 0:
+                tp+=1
+                x[-1] = 0
+                x.append(0)
+            else:
+                fp+=1
+                x[-1] = 1
+                x.append(1)
+        else:
+            if vote >= 0:
+                fn+=1
+                x[-1] = 0
+                x.append(2)
+            else:
+                tn+=1
+                x[-1] = 1
+                x.append(0)
+        class_list.append(x)
+        count+=1
+        
+    testing_file.close()
+    
+    print("Positive examples:",end=' ')
+    print(tp+fp)
+    print("Negative examples:",end=' ')
+    print(tn+fn)
+    print("False positives:",end=' ')
+    print(fp)
+    print("False negatives:",end=' ')
+    print(fn)
 
 if __name__ == '__main__':
     if len(argv) == 6:
-        W = verbose_classifier(argv[4], int(argv[2]), int(argv[3]))
-        #test_classifier(argv[2], W)
+        W = train_classifier(argv[4], int(argv[2]), int(argv[3]))
+        verbose_test_classifier(argv[5], W)
+    elif len(argv) == 5:
+        W = train_classifier(argv[3], int(argv[1]), int(argv[2]))
+        normal_test_classifier(argv[4], W)
     else: 
         print('Please input the testfile and the training file')
